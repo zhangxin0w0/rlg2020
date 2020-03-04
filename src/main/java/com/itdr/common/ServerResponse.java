d87f4d51dd8c7f41163a94f08afa75c11ae38b03
AirZhang <36889712+zhangxin0w0@users.noreply.github.com>
2020-03-04 18:54:49 +0800

    统一返回对象
    更新

---
+++ src/main/java/com/itdr/common/ServerResponse.java
@@ -0,0 +1,88 @@
+package com.itdr.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.itdr.config.ConstCode;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * ClassName: ResponseCode
+ * 日期: 2020/2/18 11:02
+ *
+ * @author Air张
+ * @since JDK 1.8
+ */
+
+@Setter
+@Getter
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+public class ServerResponse<T> implements Serializable {
+    private Integer status;
+    private T data;
+    private String msg;
+
+    private ServerResponse(Integer status) {
+        this.status = status;
+    }
+
+    private ServerResponse(Integer status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    private ServerResponse(Integer status, T data, String msg) {
+        this.status = status;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    private ServerResponse(Integer status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    private ServerResponse(String msg) {
+        this.status = ConstCode.DEFAULT_FAIL;
+        this.msg = msg;
+    }
+
+
+    public static <T> ServerResponse successRS() {
+        return new ServerResponse(ConstCode.DEFAULT_SUCCESS);
+    }
+
+    public static <T> ServerResponse successRS(String msg) {
+        return new ServerResponse(ConstCode.DEFAULT_SUCCESS,msg);
+    }
+
+    public static <T> ServerResponse successRS(T data) {
+        return new ServerResponse(ConstCode.DEFAULT_SUCCESS,data);
+    }
+    public static <T> ServerResponse successRS(Integer status,T data) {
+        return new ServerResponse(status,data);
+    }
+
+    //    成功的时候传入状态码、数据、信息
+    public static <T> ServerResponse successRS(Integer status,T data, String msg) {
+        return new ServerResponse(status,data, msg);
+    }
+
+    public static <T> ServerResponse defeatedRS(Integer errorCode, String errorMessage) {
+        return new ServerResponse(errorCode, errorMessage);
+    }
+
+    public static <T> ServerResponse defeatedRS(String errorMessage) {
+        return new ServerResponse(ConstCode.DEFAULT_FAIL,errorMessage);
+    }
+
+    @JsonIgnore
+    //让success不在json序列化结果之中
+    public boolean isSuccess() {
+        return this.status == ConstCode.DEFAULT_SUCCESS;
+    }
+
+}
